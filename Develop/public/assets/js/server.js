1059470e676ebc78edf711b951f0c739e0e9376c
Ash <acst52@gmail.com>
2023-02-17 13:45:08 -0500

    created server.js file and installed pkgs

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,8 @@
+// first lets import the modules we will be using:
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const PORT = 3001;
+
