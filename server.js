8c396cc6830ff1e686bdfa1809875ed6a1306fd6
Ash <acst52@gmail.com>
2023-02-21 15:42:47 -0500

    Removed util & updated comments

--- server.js
+++ server.js
@@ -2,14 +2,11 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-// const util = require('util');
 
 // Helper method for generating unique ids
-    // npm i uuid@3.4.0 instead!
-const uuid = require('uuid/v1');
+const uuid = require('uuid/v1'); // OR [npm i uuid@3.4.0] instead!
 
-// Heroku will create env
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; // Heroku will create .env
 
 // call the express method, assign to var "app":
 const app = express();
@@ -28,7 +25,8 @@ app.get('/notes', (req, res) =>
 
 // normally, we use JSON method to create classic API obj output:
     // where will the notes be stored? in the database JSON file. 
-    // Use sendFile method: when working w/ static files, want to give absolute path. Use path module (imported at top) join method which joins 2 strings to form a path / route. console.log __dirname --> gives us path to directory we are working on. then we can join it to the relative path = currentDir/fileName.  
+    // Use sendFile method: when working w/ static files, want to give absolute path
+
 // GET route for notes page:
 app.get('/api/notes', (req, res) => {
   fs.readFile("./db/db.json", "utf-8", (err, data) => {
@@ -77,6 +75,7 @@ app.post('/api/notes', (req, res) => {
     }
   });
 
+// DELETE route for deleting notes
 app.delete("/api/notes/:id", (req, res) =>{
   let id = req.params.id;
   fs.readFile("./db/db.json", "utf-8", (err, data) => {
@@ -84,7 +83,8 @@ app.delete("/api/notes/:id", (req, res) =>{
       console.log(err);
     } 
     let noteData = JSON.parse(data);
-    // filter thru array of note data (above), filter gives new array based on predicate fcn: keeps note in array if it meets condition: id = must match req.params id --> new array has all notes except one we want to delete
+    // now lets filter thru array of note data (above); filter gives new array based on predicate fcn: 
+      // removes note in array if meets condition: id must match req.params id --> new array has all notes except deleted
     let filteredNotes = noteData.filter((note) => note.id !== id);
     fs.writeFile("./db/db.json", JSON.stringify(filteredNotes), (writeErr) => {
       if (writeErr) {
@@ -97,7 +97,7 @@ app.delete("/api/notes/:id", (req, res) =>{
   res.end();
 })
 
-// wildcard route in case someone goes to a bad path --> index.html
+// wildcard route (in case someone goes to a bad path) --> index.html
   app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
@@ -108,27 +108,15 @@ app.listen(PORT, () =>
 );
 
 
-
-// ***** HEROKU *****
-// Heroku helps us host our APIs
-// we can take our code base, push to GitHub, then connect Heroku to GitHub so that latest commit is deployed to our project.
-// heroku has its own CLI .. cant host node.js on github pages b/c not yet supported
-// heroku --version should tell you version if its installed properly
-  // for auth run: heroku auth:login
-  // to run page: npm start 
-  // then can do git add, it commit -m "initial commit" ETC
-  // to create heroku page, type: heroku create
-  // then do git remote -v
-      // to add a remote to local repo, do: heroku git:remote
-    // gens random name for URL
-    // then check to see if git has remote pulls: git remote -v
-    // now instead of git push origin main, we do git push heroku main (to push everything to our heroku acct)
-    // heroku open (deploys page @ the herokuappdeployment URL)
-  // heroku logs --tail
-// npm install -g heroku. heroku --version. heroku auth:login. heroku create (creates link). git push heroku main. heroku open (opens deployed app from CLI)
-  // make sure git is initialized. heroku auth:login. heroku create (creates link). git push heroku main. heroku open (opens deployed app from CLI)
-  // config vars in heroku, can add API keys. in our code if we have process.env.APIKEY --> heroku will find it in config vars
-
-
-  // git push heroku main
-  // should be adding 2 more remove -v links 
+// DEPLOYING FINISHED GITHUB PROJECT ON HEROKU - STEPS:
+    // 1. make sure all your files are in main. Move contents of develop dir into main if needed.
+          // Similarly to how GitHub needs an index file in main, Heroku needs server.js in main to deploy!
+    // 2. make sure all dependencies installed (incl [npm i -g heroku] if not yet done; 
+          // [heroku --version] should show version after you install).
+    // 3. make sure working tree clean: [git add .] / [git commit -m "msg"] / [git push origin main]
+    // 4. [heroku auth:login]
+    // 5. [heroku create]
+    // 6. [git remote -v] --> SHOULD SEE 4 LINKS (fetch & push for origin and heroku)
+    // 7. [git push heroku main]
+    // 8. [heroku open] --> deploys your project @ random link generated w/ heroku create!
+    // 9. IF YOU MAKE CHANGES YOU WANT DEPLOYED: do steps 3, 4, 7
